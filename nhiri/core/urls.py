dc719eb5e4b85754842e2e99438e68df9d14383b
Ryan Grieve <me@ryangrieve.com>
2013-05-21 22:04:41 +0100

    twitter urls

--- nhiri/core/urls.py
+++ nhiri/core/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from nhiri.core.views.auth import TwitterAuthView
+from nhiri.core.views import twitter
 
 
 urlpatterns = patterns(
     '',
-    url(r'^auth/twitter', TwitterAuthView.as_view())
+    url(r'^twitter/auth', twitter.AuthView.as_view()),
+    url(r'^twitter/fetch', twitter.FetchView.as_view())
 )
