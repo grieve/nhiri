1362c9b239d3ca7d4c7cfbced84af144a6c2e56c
Ryan Grieve <me@ryangrieve.com>
2013-05-21 23:30:40 +0100

    adding api views

---
+++ nhiri/core/views/api.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from rest_framework import permissions
+
+from nhiri.core import models
+from nhiri.core import serializers
+
+
+class MomentList(generics.ListCreateAPIView):
+    model = models.Moment
+    serializers = serializers.MomentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class CaffeineMomentList(generics.ListCreateAPIView):
+    model = models.CaffeineMoment
+    serializers = serializers.CaffeineMomentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+`
