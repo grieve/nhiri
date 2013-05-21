33a35ec428bb671aaa024e4570b8674ebda61da6
Ryan Grieve <me@ryangrieve.com>
2013-05-21 22:44:11 +0100

    adding urls for gps

--- nhiri/core/urls.py
+++ nhiri/core/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, url
+
+from nhiri.core import views
 from nhiri.core.views import twitter
 
 
 urlpatterns = patterns(
     '',
     url(r'^twitter/auth', twitter.AuthView.as_view()),
-    url(r'^twitter/fetch', twitter.FetchView.as_view())
+    url(r'^twitter/fetch', twitter.FetchView.as_view()),
+    url(r'^gps/fetch', views.GPSLoggerFetchView.as_view())
 )
