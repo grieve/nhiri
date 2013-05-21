7d7d36647181b7d4281f79a2b36208a3d746c679
Ryan Grieve <me@ryangrieve.com>
2013-05-21 23:30:47 +0100

    adding api urls

--- nhiri/core/urls.py
+++ nhiri/core/urls.py
@@ -2,11 +2,16 @@ from django.conf.urls import patterns, url
 
 from nhiri.core import views
 from nhiri.core.views import twitter
+from nhiri.core.views import api
 
 
 urlpatterns = patterns(
     '',
     url(r'^twitter/auth', twitter.AuthView.as_view()),
     url(r'^twitter/fetch', twitter.FetchView.as_view()),
-    url(r'^gps/fetch', views.GPSLoggerFetchView.as_view())
+    url(r'^gps/fetch', views.GPSLoggerFetchView.as_view()),
+
+    # API URLs
+    url(r'api/moments', api.MomentList.as_view()),
+    url(r'api/caffeine', api.CaffeineList.as_view())
 )
